8e0f1636814593c5b815f240a5bd80f7fb12fe3d
Martynas Januškauskas <m.januskauskas@momook.com>
2015-11-01 22:56:49 +0200

    Added states loop definition in rules

--- src/UnyInterpreter/UnyRuleParser.php
+++ src/UnyInterpreter/UnyRuleParser.php
@@ -31,40 +31,77 @@ class UnyRuleParser
             if (empty($lines[$i])) {
                 $this->error($lines[$i - 1], $i + 1, 'init state id');
             }
-            $transition['init'] = trim($lines[$i]);
-            ++$i;
 
-            // target state
-            if (empty($lines[$i])) {
-                $this->error($lines[$i - 1], $i + 1, 'target state id');
-            }
-            $transition['target'] = trim($lines[$i]);
-            ++$i;
+            // special cases
+            // LOOP
+            if (substr($lines[$i], 0, 5) == 'LOOP ') {
+                $loop_info = explode(' ', $lines[$i], 3);
+                $loop_init_state = $loop_info[1];
+                $loop_title = $loop_info[2];
 
-            // condition
-            if (empty($lines[$i])) {
-                $this->error($lines[$i - 1], $i + 1, 'transition condition');
-            }
-            $transition['condition'] = trim($lines[$i]);
-            ++$i;
-            if ($i == $c) {
-                break;
-            }
+                $enter = $lines[$i + 1];
+                $repeat = $lines[$i + 2];
+                $exit = $lines[$i + 3];
 
-            // callback
-            if (empty($lines[$i])) {
-                $transition['callback'] = null;
+                // enter transition
+                $this->transitions[$loop_init_state][$enter] = [
+                    'target' => $loop_title,
+                    'callback' => null,
+                ];
+
+
+                $this->transitions[$loop_title] = [
+                    // repeat transition
+                    $repeat => [
+                        'target' => $loop_title,
+                        'callback' => null,
+                    ],
+                    // exit transition
+                    $exit => [
+                        'target' => $loop_init_state,
+                        'callback' => null,
+                    ],
+                ];
+
+                $i += 4;
+
+            // general case
             } else {
-                $transition['callback'] = trim($lines[$i]);
+                $transition['init'] = trim($lines[$i]);
+                ++$i;
+
+                // target state
+                if (empty($lines[$i])) {
+                    $this->error($lines[$i - 1], $i + 1, 'target state id');
+                }
+                $transition['target'] = trim($lines[$i]);
+                ++$i;
+
+                // condition
+                if (empty($lines[$i])) {
+                    $this->error($lines[$i - 1], $i + 1, 'transition condition');
+                }
+                $transition['condition'] = trim($lines[$i]);
+                ++$i;
+                if ($i == $c) {
+                    break;
+                }
+
+                // callback
+                if (empty($lines[$i])) {
+                    $transition['callback'] = null;
+                } else {
+                    $transition['callback'] = trim($lines[$i]);
+                }
+
+                $this->states[$transition['init']] = 1;
+                $this->states[$transition['target']] = 1;
+
+                $this->transitions[$transition['init']][$transition['condition']] = [
+                    'target' => $transition['target'],
+                    'callback' => $transition['callback'],
+                ];
             }
-
-            $this->states[$transition['init']] = 1;
-            $this->states[$transition['target']] = 1;
-
-            $this->transitions[$transition['init']][$transition['condition']] = [
-                'target' => $transition['target'],
-                'callback' => $transition['callback'],
-            ];
 
             // skip empty lines
             ++$i;
@@ -78,6 +115,7 @@ class UnyRuleParser
             }
         }
 
+        print_R($this->transitions);die;
         $this->states = array_keys($this->states);
     }
 
